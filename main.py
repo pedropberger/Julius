6247e58144db952a2c42f42e407db5d675b7c29c
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2022-06-17 09:55:53 -0300

    restarting implementations

--- main.py
+++ main.py
@@ -3,3 +3,31 @@
 ##testing commit
 
 ##Test2
+
+
+
+import _thread
+import threading
+import logging
+import csv
+import requests
+import json
+from unicodedata import normalize
+from bs4 import BeautifulSoup
+
+
+# def getDados(cidade, linkAPI, poder):
+#     try:
+#         print(linkAPI + '/api/pessoal/servidor?exercicio=2020&mes=01')
+#         resultado = requests.get(linkAPI + '/api/pessoal/servidor?exercicio=2020&mes=01')
+#
+#         j = json.loads(resultado.text)
+#
+#         # print(resultado)
+#         # soup = BeautifulSoup(resultado.content)
+#         # print(soup.find("a", href=True)['href'])
+#         # csv = requests.get(soup.find("a", href=True)['href'])
+#         # print(csv.content)
+
+resultado = request.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano=2017&mes=1')
+j = json.loads(resultado.text)
