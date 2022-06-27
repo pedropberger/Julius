ca3f2976b2019fdc1bb33d38d879651bea7d3eda
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2022-06-27 00:11:15 -0300

    restarting implementations strugles with pychamr

--- Extract/data.py
+++ Extract/data.py
@@ -1,15 +1,16 @@
-##### All starts here
+X'import json
+import requests
+import xmltodict
+import pandas as pd
 
-#import json
-#import requests
-#import xmltodict
-#import pandas as pd
-from Extract.data import Data
+class Data():
+    def extract():
+        """This function extract the xml files from the Transparency Portals.
+        Usually they come in json format, but in some cases they come in xml with an embedded json.
+        After extraction they are converted into tables for later storage or analysis."""
 
-def main():
-    print("Partiu!")
-    Data.extract()
-
-
-if __name__ == "__main__":
-    main()
+        res = requests.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano=2022&mes=1')
+        #this line bellow can be used if u want extract and work with a Json file.
+        #jsonfile=(res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')
+        x=pd.read_json((res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>'))
+        return print(x)
--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 ##### All starts here
 
-##testing commit
+#import json
+#import requests
+#import xmltodict
+#import pandas as pd
+from Extract.data import Data
 
-##Test2
-
-
-
-import _thread
-import threading
-import logging
-import csv
-import requests
-import json
-from unicodedata import normalize
-from bs4 import BeautifulSoup
+def main():
+    print("Partiu!")
+    Data.extract()
 
 
-# def getDados(cidade, linkAPI, poder):
-#     try:
-#         print(linkAPI + '/api/pessoal/servidor?exercicio=2020&mes=01')
-#         resultado = requests.get(linkAPI + '/api/pessoal/servidor?exercicio=2020&mes=01')
-#
-#         j = json.loads(resultado.text)
-#
-#         # print(resultado)
-#         # soup = BeautifulSoup(resultado.content)
-#         # print(soup.find("a", href=True)['href'])
-#         # csv = requests.get(soup.find("a", href=True)['href'])
-#         # print(csv.content)
-
-resultado = request.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano=2017&mes=1')
-j = json.loads(resultado.text)
+if __name__ == "__main__":
+    main()
