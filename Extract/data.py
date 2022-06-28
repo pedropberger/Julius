2cad4af096465a9ffefffdeb018664563c742555
Pedrow <pedropberger@gmail.com>
2022-06-28 15:26:00 -0300

    Loops and appends for licitations

--- Extract/data.py
+++ Extract/data.py
@@ -9,7 +9,9 @@ import os
 import sqlite3
 import datetime
 
-#executando configurações
+#Loading configs
+initialdate = []
+finaldate = []
 exec(open("config.py").read())
 
 class Data():
@@ -22,7 +24,6 @@ class Data():
 
         #Cleaning old DB
         os.remove("C:\TempData\Julius.db") if os.path.exists("C:\TempData\Julius.db") else None
-        #os.remove("C:\TempData\licitacoes.db") if os.path.exists("C:\TempData\licitacoes.db") else None
 
         #Start connection and cursor setup
         conn = sqlite3.connect('C:\TempData\Julius.db')
@@ -32,41 +33,55 @@ class Data():
         print('Database Connected!')
 
         #Create table --- In the future we need to transfer that for a DB application
-        sql_create = 'create table licitacoes '\
-        '(ano	decimal(4), '\
-        'mes	nvarchar(20), '\
-        'tipo_processo	nvarchar(20), '\
-        'unidade_gestora	nvarchar(100), '\
-        'modalidade	nvarchar(500), '\
-        'licitacao	nvarchar(30), '\
-        'processo	nvarchar(30), '\
-        'objeto	texto	nvarchar(300), '\
-        'abertura	datetime, '\
-        'homologacao	datetime, '\
-        'conclusao	datetime, '\
-        'situacao	nvarchar(20), '\
-        'valor_homologado	decimal(10,2))'
+        # sql_create = 'create table licitacoes '\
+        # '(ano	decimal(4), '\
+        # 'mes	nvarchar(20), '\
+        # 'tipo_processo	nvarchar(20), '\
+        # 'unidade_gestora	nvarchar(100), '\
+        # 'modalidade	nvarchar(500), '\
+        # 'licitacao	nvarchar(30), '\
+        # 'processo	nvarchar(30), '\
+        # 'objeto	texto	nvarchar(300), '\
+        # 'abertura	datetime, '\
+        # 'homologacao	datetime, '\
+        # 'conclusao	datetime, '\
+        # 'situacao	nvarchar(20), '\
+        # 'valor_homologado	decimal(10,2))'
 
-        cur.execute(sql_create)
+        #Create dataframe
+        #dflicitacoes = pd.DataFrame(columns= ('ano', 'mes', 'tipo_processo', 'unidade_gestora', 'modalidade', 'licitacao', 'processo', 'objeto', 'abertura', 'homologacao', 'conclusao', 'situacao', 'valor_homologado'))
+
+        #cur.execute(sql_create)
 
         print('Database Created!')
 
-        for ano in range(initialdate.year, finaldate.year):
-            res = requests.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano='+str(ano)+'&mes=1')
+        for ano in range(initialdate.year, (finaldate.year-2)):
+            for mes in range(1,13):
+                res = requests.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano='+str(ano)+'&mes='+str(mes))
 
-            """this line bellow can be used if u want extract and work with a Json file:
-            jsonfile=(res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')"""
+                """this line bellow can be used if u want extract and work with a Json file:
+                jsonfile=(res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')"""
 
-            #Extract -> convert xlm to Json -> read JSon as a table -> convert table in Dataframe
-            dflicitacoes=pd.DataFrame(pd.read_json((res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').\
-                replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')))
-            print('Dataframe created!')
-            
-            #Update (load) table to DB
-            tablename = ('licitacoes' + str(ano))
+                #Extract -> convert xlm to Json -> read JSon as a table -> convert table in Dataframe
+                dflicitacoes=pd.DataFrame(pd.read_json((res.text).replace('<?xml version="1.0" encoding="utf-8"?>','').\
+                    replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')))
+                print('Dataframe created!')
 
-            dflicitacoes.to_sql(tablename, conn, if_exists='replace', index = False)
-            print('Data loaded to sql!')
+                #Create table in DB
+                tablename = ('licitacoes' + 'SMJ')
+
+                #Check empty values and store (load) table in DB
+                if dflicitacoes.empty:
+                    print('Data ' + 'licitacoes' + str(ano) + "/" + str(mes) + ' is empty!')
+                else:
+                    dflicitacoes.to_sql(tablename, conn, if_exists='append', index = False)
+                    #dflicitacoes=pd.concat([dflicitacoes, dflicitacoesaux])
+                    print('Data ' + 'licitacoes' + str(ano) + "/" + str(mes) + ' appended')
+
+        # print(dflicitacoesaux)
+        # print(dflicitacoes)
+        # dflicitacoes.to_sql(tablename, conn, if_exists='replace', index = False)
+        print('Data loaded to sql!')
 
         #Test Code
         #cur.execute('''  
