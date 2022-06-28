c950f4c6949deeca3c787d91c75b253724c35db4
Pedrow <pedropberger@gmail.com>
2022-06-28 15:27:06 -0300

    ajusts in configs

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from dateutil.relativedelta import relativedelta
 initialdate = date(2019,1,1)
 finaldate = date.today() + relativedelta(months=-2)
 print('Data inicial de busca: ' + str(initialdate) + ' Ano-Mês-Dia')
-print('Data final de busca: ' + str(finaldate) + '\n Você pode alterar a data final no arquivo config.py' + '\n por padrão a busca ocorre com dois meses de defasagem da data atual')
+print('Data final de busca: ' + str(finaldate))
 
 ######Database Configs
 mysql = {
