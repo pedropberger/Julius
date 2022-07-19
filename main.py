a474f6003a404b2173a9877f0928e4b2d0fa9214
Pedrow <pedropberger@gmail.com>
2022-07-19 14:50:54 -0300

    Pt-br notes

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ We catalog them in 4 diferent patterns to extract data in a efficient way.
 
 You have 2 ways to execute this code. Using threads or not.
 If you choose use threading you can select how API methods (data clusters) u want extract.
-A full extract consumes 4gb of you hard disk."""
+A full extract consumes 4gb of you hard disk.
+
+###############################################################
+
+Bem-vindo à Julius!
+
+Este é um código simples criado para extrair dados de todos os Portais de Transparência da Cidade do Estado do Espírito Santo, Brasil.
+Cada portal tem sua própria API, mas têm padrões semelhantes (depende da empresa que os cria).
+Nós os catalogamos em 4 padrões diferentes para extrair dados de uma maneira eficiente.
+
+Você tem 2 maneiras de executar este código. Usando ou não threads.
+Se você escolher usar threading você pode selecionar como os métodos API (clusters de dados) você quer extrair.
+Um extrato completo consome 4gb de seu disco rígido.
+
+Todos comentários estão em inglês pq a ideia é esse projeto também ser uma ferramenta de aprendizado e prática
+
+Insira a API da sua cidade e colabore para unificar as fontes de dados públicos no Brasil"""
 
 """All starts here"""
 
