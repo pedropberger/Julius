05d610aada1b841355d4d75c7e83eceed0bcbbc1
Pedrow <pedropberger@gmail.com>
2022-07-26 18:18:51 -0300

    start scraping

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,32 @@
 """Here we test stuff"""
 
+
+# Import libraries
+import requests
+from bs4 import BeautifulSoup
+
+import scrapy
+
+
+
+# Collect and parse first page
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Pull all text from the BodyText div
+artist_name_list = soup.find(class_='BodyText')
+
+# Pull text from all instances of <a> tag within BodyText div
+artist_name_list_items = artist_name_list.find_all('a')
+print(artist_name_list)
+
+
+# Create for loop to print out all artists' names
+for artist_name in artist_name_list_items:
+    print(artist_name.prettify())
+
+
+
 # class DataPortaltp():
 
 #     def extractall():
